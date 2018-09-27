08cfb14b901beaae75434825dfe212c9c5a84bc9
Nicolaudie <adam.smith@nicolaudiegroup.com>
2018-09-27 15:30:51 +0100

    Updated to swift 4.2

--- Classes/DragableTableExtension.swift
+++ Classes/DragableTableExtension.swift
@@ -358,10 +358,10 @@ private class _DisplayLink{
             _link.isPaused = newValue
         }
     }
-    fileprivate init (_ callback: @escaping (Void) -> Void) {
+    fileprivate init (_ callback: @escaping () -> Void) {
         _callback = callback
         _link = CADisplayLink(target: _DisplayTarget(self), selector: #selector(_DisplayTarget._callback))
-        _link.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
+        _link.add(to: RunLoop.main, forMode: RunLoop.Mode.common)
         _link.isPaused = true
     }
     
